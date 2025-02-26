3f81698c24a18ecfa67688460bdfe09b56fb5a99
MizuhoNK <mizuho.ymd2979@gmail.com>
2025-02-26 17:14:26 +0900

    課題の提出

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP基礎_課題7</title>
+</head>
+<body>
+    <?php
+    //配列に値を入れる
+    $grocery = ['name' => 'onion','price' => 200, 'weight' => 160 ];
+
+    //配列を出力する
+    print_r($grocery)
+    ?>
+</body>
+</html>
